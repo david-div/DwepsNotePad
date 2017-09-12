2696f5cf3c4ac6396d38b13daaa6d1e82a96e1e5
stephengeller <stephen.geller@hotmail.com>
2017-09-12 13:23:10 +0100

    added test for multiple blog posts

--- src/notePad.js
+++ src/notePad.js
@@ -4,6 +4,8 @@ var textField = document.getElementById('text-field');
 var blogList = document.getElementById('blog-list');
 
 button.addEventListener("click", function(){
-    blogList.innerHTML = textField.value;
+    // blogList.innerHTML = textField.value;
+    var htmlString = '<li>'+ textField.value + '</li>'
+    blogList.insertAdjacentHTML("beforeend", htmlString);
     textField.value = ''
 });
--- test/notePadTest.js
+++ test/notePadTest.js
@@ -1,49 +1,45 @@
-//circle test example
+var test = function() {
 
-var button = document.getElementById('submit-button');
-var textField = document.getElementById('text-field');
-var blogList = document.getElementById('blog-list');
+    var button = document.getElementById('submit-button');
+    var textField = document.getElementById('text-field');
+    var blogList = document.getElementById('blog-list');
 
-var success = function () {
-    console.log('Test passed')
+    var success = function () {
+        console.log('Test passed')
+    };
+
+    var fail = function () {
+        console.log('Test failed')
+    };
+
+    textField.value = 'a';
+
+    if (textField.value === 'a') {
+        success();
+    }
+
+    if (button) {
+        success();
+    } else {
+        fail();
+    }
+
+    textField.value = 'a';
+    button.click();
+    if (blogList.innerHTML.includes('a')) {
+        success();
+    } else {
+        fail()
+    }
+
+    textField.value = 'a';
+    button.click();
+    textField.value = 'b';
+    button.click();
+
+    if (blogList.innerHTML.includes('a') && blogList.innerHTML.includes('b')) {
+        success();
+    } else {
+        fail()
+    }
 };
-
-var fail = function () {
-    console.log('Test failed')
-};
-
-textField.value = 'a';
-
-if (textField.value === 'a') {
-  success();
-}
-
-if (button) {
-  success();
-} else {
-  fail();
-}
-
-textField.value = 'a';
-button.click()
-if (blogList.innerHTML.includes('a')){
-    success();
-}else{
-  fail()
-}
-
-
-
-
-
-
-// function testCircleRadiusDefaultsTo10() {
-//   var circle = new Circle();
-//   assert.isTrue(circle.radius === 10);
-// };
-//
-// testCircleRadiusDefaultsTo10();
-
-
-
-
