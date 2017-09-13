ccd4ebecb92d2e876cfce1ad6f670ec053a998da
pyan83 <puyanwei@gmail.com>
2017-09-13 13:12:32 +0100

    adding specrunner to notepadTest

--- test/notePadTest.js
+++ test/notePadTest.js
@@ -1,5 +1,32 @@
 var test = function() {
 
+  var addInput = function(parentId, elementTag, elementId, html) {
+    var container = document.getElementById('parent');
+    var input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'text-field');
+    container.appendChild(input);
+  };
+
+  var addButton = function(parentId, elementTag, elementId, html) {
+    var container = document.getElementById('parent');
+    var button = document.createElement('button');
+    button.setAttribute('id', 'submit-button');
+    button.innerHTML = "submit";
+    container.appendChild(button);
+  };
+
+  var addblogList = function(parentId, elementTag, elementId, html) {
+    var container = document.getElementById('parent');
+    var blogList = document.createElement('ul');
+    blogList.setAttribute('id', 'blog-list');
+    container.appendChild(blogList);
+  };
+  addButton();
+  addInput();
+  addblogList();
+
+
     var button = document.getElementById('submit-button');
     var textField = document.getElementById('text-field');
     var blogList = document.getElementById('blog-list');
@@ -8,12 +35,12 @@ var test = function() {
 
     var success = function () {
         console.log("%cTest passed", 'color:green');
-        // testResult.insertAdjacentHTML("beforeend", '<li style="color: green"> Test passed! </li>');
+        testResult.insertAdjacentHTML("beforeend", '<li style="color: green"> Test passed! </li>');
     };
 
     var fail = function () {
         console.log("%cTest failed", 'color:red');
-        // testResult.insertAdjacentHTML("beforeend", '<li style="color: red"> Test failed! </li>');
+        testResult.insertAdjacentHTML("beforeend", '<li style="color: red"> Test failed! </li>');
     };
 
     var reset = function() {
@@ -75,3 +102,21 @@ var test = function() {
 };
 
 test();
+//
+// function removeInput(elementId) {
+//   var input = document.getElementById('text-field');
+//   input.parentNode.removeChild(input);
+// }
+// removeInput();
+//
+// function removeButton(elementId) {
+//   var button = document.getElementById('submit-button');
+//   button.parentNode.removeChild(button);
+// }
+// removeButton();
+//
+// function removeBloglist(elementId) {
+//   var bloglist = document.getElementById('blog-list');
+//   bloglist.parentNode.removeChild(bloglist);
+// }
+// removeBloglist();
